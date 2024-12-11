3f09290ba5637352ee33d0a3e6e69138fa5950eb
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-12-11 22:51:22 +0530

    will do tomorrow

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+        {/* Left Side */}
+        <div>
+
+        </div>
+        {/* Right Side */}
+        
+    </div>
+  )
+}
+export default Header
