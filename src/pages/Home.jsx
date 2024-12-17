e1ef8b08314722a4be9de822fccb9edd66fdc055
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-12-17 17:50:28 +0530

    slider upload created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import BgSlider from "../components/bgSlider"
 import Header from "../components/Header"
 import Steps from "../components/Steps"
+import Upload from "../components/Upload"
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Steps />
+      <BgSlider />
+      <Upload />
     </div>
   )
 }
